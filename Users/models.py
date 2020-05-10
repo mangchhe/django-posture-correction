f61aa0ed2e7879f7cb61d8be661a1dddfc2d862a
stone <sysh58@naver.com>
2020-05-10 12:06:46 +0900

    Update models.py

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_ID = models.CharField(max_length = 50)
+    user_password = models.CharField(max_length = 50)
+    user_name = models.CharField(max_length = 50)
+    user_birth = models.DateField()
