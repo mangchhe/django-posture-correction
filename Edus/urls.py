ea3a79453e33b30d32494fcb667f8b2599855794
HaJooNoteBook <mangchhe@naver.com>
2020-05-17 21:16:23 +0900

    결과 리스트 페이징 목록 구현

--- Edus/urls.py
+++ Edus/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from Edus import views
 
 urlpatterns = [
-    path('playView', views.play, name = 'playView'),
+    path('playView/<int:page_no>', views.play, name = 'playView'),
     path('video_feed', views.video_feed, name='video_feed'),
 
     path('mypageView', views.mypage, name = 'mypageView'),
--- Edus/views.py
+++ Edus/views.py
@@ -1,37 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http.response import StreamingHttpResponse
 from Edus.camera import VideoCamera
 from .models import EdusDB
 from Users.models import UsersDB
 from Videos.models import VideosDB
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 # 모드 선택 후 화면
 
-def play(request):
+def play(request, page_no):
 
 	# 비디오 정보 (mp4, avi 등)
 
-	edu = EdusDB.objects.filter(video_id = 1, user_id = 1)	# 해당 영상과, 사용자 주
+	edu = EdusDB.objects.filter(video_id = 1, user_id = 1).order_by('-edu_days') # 해당 영상과, 사용자 주
+
+	eduList = Paginator(edu, 4)
 
 	days = []
 	video = []
 
-	for i in edu.values():
+	totalPageList = [i for i in range(1, eduList.num_pages + 1)]
+	currentPage = page_no
+
+	for i in eduList.get_page(page_no).object_list.values():
 
 		video.append(i['recode_video'])
 		days.append(i['edu_days'])
 
 	context = {
-		'a' : zip(video, days),
+		'videoList' : zip(video, days),
+		'totalPageList' : totalPageList,
+		'currentPage' : currentPage,
 	}
 
 	if request.method == 'POST':
 		new_video = EdusDB.objects.create(
 			# edu_days = 현재 시간 자동 저장
 			video_id=VideosDB.objects.all()[0],
-			user_id=UsersDB.objects.all()[1],
+			user_id=UsersDB.objects.all()[0],
 			recode_video='test',
 			score=99
 		)
@@ -57,4 +65,3 @@ def video_feed(request):
 def mypage(request):
 
 	return render(request, 'mypageView.html')
-
