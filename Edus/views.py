94c47d9797934bb645eaec611965180d7d63dcca
HaJooNoteBook <mangchhe@naver.com>
2020-05-17 18:59:09 +0900

    실전 / 연습 결과 업로드, 결과 리스트 날짜, 영상 값 불러오기 구현

--- Edus/views.py
+++ Edus/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 from Edus.camera import VideoCamera
+from .models import EdusDB
+from Users.models import UsersDB
+from Videos.models import VideosDB
 
 # Create your views here.
 
@@ -10,7 +13,30 @@ def play(request):
 
 	# 비디오 정보 (mp4, avi 등)
 
-    return render(request, 'playView.html')
+	edu = EdusDB.objects.filter(video_id = 1, user_id = 1)	# 해당 영상과, 사용자 주
+
+	days = []
+	video = []
+
+	for i in edu.values():
+
+		video.append(i['recode_video'])
+		days.append(i['edu_days'])
+
+	context = {
+		'a' : zip(video, days),
+	}
+
+	if request.method == 'POST':
+		new_video = EdusDB.objects.create(
+			# edu_days = 현재 시간 자동 저장
+			video_id=VideosDB.objects.all()[0],
+			user_id=UsersDB.objects.all()[1],
+			recode_video='test',
+			score=99
+		)
+
+	return render(request, 'playView.html', context)
 
 def gen(camera): # https://item4.blog/2016-05-08/Generator-and-Yield-Keyword-in-Python/
 # 앨범 이미지
