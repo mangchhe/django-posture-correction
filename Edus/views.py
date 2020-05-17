a47a340d715f723d0d10afdbb4cfa6445ac38db3
HaJooNoteBook <mangchhe@naver.com>
2020-05-17 23:00:02 +0900

    실전 / 연습 결과창에서 시간 경과 후 등급, 점수, 날짜 연산 후 출력 구현 따라할 동영상 구현

--- Edus/views.py
+++ Edus/views.py
@@ -5,6 +5,7 @@ from .models import EdusDB
 from Users.models import UsersDB
 from Videos.models import VideosDB
 from django.core.paginator import Paginator
+import datetime
 
 # Create your views here.
 
@@ -18,19 +19,21 @@ def play(request, page_no):
 
 	eduList = Paginator(edu, 4)
 
+	idx = []
 	days = []
 	video = []
 
 	totalPageList = [i for i in range(1, eduList.num_pages + 1)]
 	currentPage = page_no
 
-	for i in eduList.get_page(page_no).object_list.values():
+	for i, j in enumerate(eduList.get_page(page_no).object_list.values()):
 
-		video.append(i['recode_video'])
-		days.append(i['edu_days'])
+		idx.append((page_no-1) * 4 + i+1)
+		video.append(j['recode_video'])
+		days.append(j['edu_days'])
 
 	context = {
-		'videoList' : zip(video, days),
+		'videoList' : zip(idx, video, days),
 		'totalPageList' : totalPageList,
 		'currentPage' : currentPage,
 	}
@@ -45,6 +48,61 @@ def play(request, page_no):
 		)
 
 	return render(request, 'playView.html', context)
+
+def play_after(request, page_no):
+
+	# 비디오 정보 (mp4, avi 등)
+
+	# after
+
+	rankList = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+	zum = 90
+	rank = 'F'
+	nowDate = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+	for idx, val in enumerate(rankList):
+
+		if zum >= 95 - idx * 5:
+
+			rank = rankList[idx]
+			break
+
+	# before
+
+	edu = EdusDB.objects.filter(video_id = 1, user_id = 1).order_by('-edu_days') # 해당 영상과, 사용자 주
+
+	eduList = Paginator(edu, 4)
+
+	idx = []
+	days = []
+	video = []
+
+	totalPageList = [i for i in range(1, eduList.num_pages + 1)]
+	currentPage = page_no
+
+	for i, j in enumerate(eduList.get_page(page_no).object_list.values()):
+
+		idx.append((page_no-1) * 4 + i+1)
+		video.append(j['recode_video'])
+		days.append(j['edu_days'])
+
+	context = {
+		'videoList' : zip(idx, video, days),
+		'totalPageList' : totalPageList,
+		'currentPage' : currentPage,
+		'result' : str(zum)+' , '+str(rank)+' , '+str(nowDate),
+	}
+
+	if request.method == 'POST':
+		new_video = EdusDB.objects.create(
+			# edu_days = 현재 시간 자동 저장
+			video_id=VideosDB.objects.all()[0],
+			user_id=UsersDB.objects.all()[0],
+			recode_video='test',
+			score=99
+		)
+
+	return render(request, 'playViewResult.html', context)
 
 def gen(camera): # https://item4.blog/2016-05-08/Generator-and-Yield-Keyword-in-Python/
 # 앨범 이미지
