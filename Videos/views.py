fe5dcbc0c6bf93b8d1bcf845d64257b432c32579
Yiseungyoon <sysh58@naver.com>
2020-06-18 16:39:46 +0900

    타이틀 이미지 추가

--- Videos/views.py
+++ Videos/views.py
@@ -22,24 +22,13 @@ def search(request):
     else:
         qs = VideosDB.objects.all()
 
-    page = int(request.GET.get('page', 1))
-    paginated_by = 5
-
     # GET request의 인자중에 q 값이 있으면 가져오고, 없으면 빈 문자열 넣기
     q = request.GET.get('q', '')
-    le = request.GET.getlist('le', '')
-    llist = ['상', '중', '하']
-    nole = []
-    if le:
-        for x in llist:
-            if str(x) not in le:
-                nole.append(x)
-
-        for x in nole:
-            qs = qs.exclude(level=x)
-
     if q:  # q가 있으면
         qs = qs.filter(title__icontains=q)  # 제목에 q가 포함되어 있는 레코드만 필터링
+
+    page = int(request.GET.get('page', 1))
+    paginated_by = 5
 
     total_count = len(qs)
     total_page = math.ceil(total_count/paginated_by)
@@ -52,7 +41,7 @@ def search(request):
 
     qs = qs[start_index:end_index]
 
-    return render(request, 'search.html', {'search': qs, 'q': q, 'where': where, 'page_range': page_range, 'le': le, })
+    return render(request, 'search.html', {'search': qs, 'q': q, 'where': where, 'page_range': page_range,})
 
 
 def main(request):
