f441d09ccfecd9fdd56a10175b496f01ba2e204c
HaJooNoteBook <mangchhe@naver.com>
2020-06-18 12:36:25 +0900

    Videos views 오류 수정

--- Videos/views.py
+++ Videos/views.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import VideosDB
@@ -6,14 +6,14 @@ from .forms import VideoForm
 from Users.models import UsersDB
 from Edus.models import EdusDB
 from django.db.models import Sum, Subquery, OuterRef
-import simplejson as json 
 import math
 
 # Create your views here.
 
+
 def search(request):
-    
-    where = request.GET.get('where',None)
+
+    where = request.GET.get('where', None)
 
     if where == 'pop':
         qs = VideosDB.objects.all().order_by('-views')
@@ -22,12 +22,12 @@ def search(request):
     else:
         qs = VideosDB.objects.all()
 
-    page = int(request.GET.get('page',1))
+    page = int(request.GET.get('page', 1))
     paginated_by = 5
 
-
-    q = request.GET.get('q', '') # GET request의 인자중에 q 값이 있으면 가져오고, 없으면 빈 문자열 넣기
-    le = request.GET.getlist('le','')
+    # GET request의 인자중에 q 값이 있으면 가져오고, 없으면 빈 문자열 넣기
+    q = request.GET.get('q', '')
+    le = request.GET.getlist('le', '')
     llist = ['상', '중', '하']
     nole = []
     if le:
@@ -37,10 +37,10 @@ def search(request):
 
         for x in nole:
             qs = qs.exclude(level=x)
-    
-    if q: # q가 있으면
-        qs = qs.filter(title__icontains=q) # 제목에 q가 포함되어 있는 레코드만 필터링
-    
+
+    if q:  # q가 있으면
+        qs = qs.filter(title__icontains=q)  # 제목에 q가 포함되어 있는 레코드만 필터링
+
     total_count = len(qs)
     total_page = math.ceil(total_count/paginated_by)
     if total_page > 1:
@@ -52,7 +52,8 @@ def search(request):
 
     qs = qs[start_index:end_index]
 
-    return render(request, 'search.html', {'search' : qs, 'q' : q, 'where':where, 'page_range':page_range, 'le':le,})
+    return render(request, 'search.html', {'search': qs, 'q': q, 'where': where, 'page_range': page_range, 'le': le, })
+
 
 def main(request):
 
@@ -64,18 +65,21 @@ def main(request):
 
     # 이용자 순위 (총 점수 합계순으로 출력)
 
-    Edus_list = EdusDB.objects.values('user_id__username').annotate(Sum('score')).order_by('-score__sum')
+    Edus_list = EdusDB.objects.values('user_id__username').annotate(
+        Sum('score')).order_by('-score__sum')
     # 인기 채널 순위 (영상 조회수가 높은 게시자 순으로 출력)
-    channel = VideosDB.objects.values('editor__username').annotate(Sum('views')).order_by('-views__sum')
+    channel = VideosDB.objects.values('editor__username').annotate(
+        Sum('views')).order_by('-views__sum')
     print(channel)
 
-    return render(request, 'index.html', {'pop' : pop, 'late' : late,'user':Edus_list,'channel':channel})
+    return render(request, 'index.html', {'pop': pop, 'late': late, 'user': Edus_list, 'channel': channel})
+
 
 def level(request):
     qs = VideosDB.objects.all()
 
-    le = request.POST.getlist('data[]','')
-    q = request.POST.get('q','')
+    le = request.POST.getlist('data[]', '')
+    q = request.POST.get('q', '')
 
     llist = ['상', '중', '하']
     nole = []
@@ -87,19 +91,20 @@ def level(request):
 
         for x in nole:
             qs = qs.exclude(level=x)
-    if q: # q가 있으면
-        qs = qs.filter(title__icontains=q) # 제목에 q가 포함되어 있는 레코드만 필터링
+    if q:  # q가 있으면
+        qs = qs.filter(title__icontains=q)  # 제목에 q가 포함되어 있는 레코드만 필터링
 
     qs = list(qs.values())
     length = len(qs)
 
     content = {
-        'q' : q,
-        'search' : qs,
-        'length' : length
+        'q': q,
+        'search': qs,
+        'length': length
     }
     return JsonResponse(content)
 
+
 def VideoShow(request, video_id):
     result = VideosDB.objects.filter(id=video_id)
-    return render(request, 'VideoShowModal.html', {'result':result})
+    return render(request, 'VideoShowModal.html', {'result': result})
